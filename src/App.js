bc9ca5c3d62baf57a405b32294269ab57cc3d8ef
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2024-12-08 02:19:54 +0530

    initial commit with responsive nav bar

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="home-container">
+        <Header />
+      </div>
+
+      <div className="work-container">
+        <div className="heading-container">
+          <h2>One-stop shop for all things interiors</h2>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+          <p>Be it end-to-end interiors, renovation or modular solutions, we have it all for your home or office. With a wide range of furniture & decor, we have your back from start to finish.</p>
+        </div>
+        <div className="word-blocks-container">
+
+
+
+        </div>
+      </div>
+
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,76 @@
+import logo from "../assets/images/logo.png";
+import data from "../assets/data/data.json";
+import { useEffect, useState } from "react";
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [showHamburgerMenu, setshowHamburgerMenu] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    setshowHamburgerMenu(!isMenuOpen);
+  };
+
+
+  useEffect(() => {
+    const navBar =  document.getElementById("home-header-container");
+  window.onscroll = function () {
+    if (navBar) {
+      if (window.scrollY > 50) {
+        navBar.classList.add("dark");
+      } else {
+        navBar.classList.remove("dark");
+      }
+    }
+  };
+  },[])
+
+
+
+  return (
+    <>
+      <div className="home-header-container" id="home-header-container">
+        <div className="header-container" id="header-container">
+          <div className="header-logo" id="header-logo">
+            <img src={logo} />
+          </div>
+
+          <div className="header-menu-items" id="header-menu-items">
+            {data?.header.navlist.map((items, i) => {
+              return <span>{items}</span>;
+            })}
+          </div>
+
+          <div className="header-menu-hamburger">
+            <div
+              className={`container ${isMenuOpen && "mobileView"}`}
+              onClick={toggleMenu}
+            >
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={`header-hamburger-container ${
+            showHamburgerMenu ? "slideIn" : "slideOut"
+          }`}
+        >
+          <div
+            className={`header-menu-hamburger-items`}
+            id="header-menu-hamburger-items"
+          >
+            {data?.header.navlist.map((items, i) => {
+              return <span>{items}</span>;
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="home-body-container">
+        <h1>Homes Designed For Tomorrow</h1>
+        <br />
+        <button className="home-button">View Our Designs</button>
+      </div>
+    </>
+  );
+};
