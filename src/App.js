442c86b3c4fa844f75d3e0ddba1d3a34e0e22efd
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2024-12-15 00:58:55 +0530

    adding google maps and customer feedback containers:2

--- src/App.js
+++ src/App.js
@@ -306,7 +306,7 @@ function App() {
             </address>
           </div>
           <div className='map'>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.3667194125474!2d78.37231208553285!3d17.44215335857927!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93001b75ba2b%3A0x72aef5ac3f2e4e9a!2sRaidurg%20Metro%20Station!5e0!3m2!1sen!2sin!4v1734203775113!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.3667194125474!2d78.37231208553285!3d17.44215335857927!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93001b75ba2b%3A0x72aef5ac3f2e4e9a!2sRaidurg%20Metro%20Station!5e0!3m2!1sen!2sin!4v1734203775113!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='location'></iframe>
           </div>
         </div>
       </div>
